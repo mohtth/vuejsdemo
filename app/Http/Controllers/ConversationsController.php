cb33583d605b96dc366e40cb8a3f8e3dd3837a46
mohtth <mtoutouhi@gmail.com>
2019-08-21 16:21:13 +0200

    amelioration et simplification du code avec repository

--- app/Http/Controllers/ConversationsController.php
+++ app/Http/Controllers/ConversationsController.php
@@ -3,21 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Repository\ConversationRepository;
+use Illuminate\Auth\AuthManager;
 use Illuminate\Foundation\Auth\User as IlluminateUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ConversationsController extends Controller
 {
+    /**
+     * @var ConversationRepository
+     */
+    private $repo;
+
+    public function __construct(ConversationRepository $conversationRepository, AuthManager $authman)
+    {
+        $this->repo = $conversationRepository;
+        $this->authman = $authman;
+    }
+
     public function index()
     {
-        $users = User::select('name', 'id')->where('id', '!=', Auth::user()->id)->get();
-        return view('conversations/index', compact('users'));
+        return view('conversations/index', [
+            'users' => $this->repo->getConversations($this->authman->user()->id)
+        ]);
     }
 
     public function show(User $user)
     {
-        $users = User::select('name', 'id')->where('id', '!=', Auth::user()->id)->get();
-        return view('conversations/show', compact('users', 'user'));
+        return view('conversations/show', [
+            'users' => $this->repo->getConversations($this->authman->user()->id),
+            'user' => $user
+        ]);
     }
 }
