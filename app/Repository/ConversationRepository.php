e817a23b5cda944a9cf60ff1c2c4a7576ec62ff6
mohtth <mtoutouhi@gmail.com>
2019-08-22 18:00:41 +0200

    alerte pas mail en place

--- app/Repository/ConversationRepository.php
+++ app/Repository/ConversationRepository.php
@@ -27,10 +27,20 @@ class ConversationRepository
 
     public function getConversations(int $userId)
     {
-        return $this->user->newQuery()
+        $conversations = $this->user->newQuery()
             ->select('name', 'id')
             ->where('id', '!=', $userId)
             ->get();
+        /*
+        $unread = $this->unreadCount($userId);
+        foreach ($conversations as $conversation){
+            if (isset($unread[$conversation->id])) {
+            }else {
+                $conversation->unread = 0;
+            }
+        }
+        */
+        return $conversations;
     }
 
     public function createMessage(string $content, int $from, int $to): Message
@@ -49,7 +59,36 @@ class ConversationRepository
             ->whereRaw("((from_id = $from AND to_id = $to) OR (from_id = $to AND to_id = $from))")
             ->orderBy('created_at', 'DESC')
             ->with([
-                'from' => function($query) {return $query->select('name', 'id'); }
+                'from' => function ($query) {
+                    return $query->select('name', 'id');
+                }
             ]);
+    }
+
+
+    /**
+     * Undocumented function
+     *
+     * @param integer $userId
+     * @return void
+     */
+    public function unreadCount(int $userId)
+    {
+        return $this->message->newQuery()
+            ->where('to_id', $userId)
+            ->groupBy('from_id')
+            ->selectRaw('from_id, COUNT(id) as count')
+            ->whereRaw('read_at IS NULL')
+            ->get()
+            ->pluck('count', 'from_id');
+    }
+
+    /**
+     * Marque tous les messages de cet utilisateur comme lu
+     * @param integer $id
+     */
+    public function readAllFrom(int $from, int $to)
+    {
+        $this->message->where('from_id', $from)->where('to_id', $to)->update(['read_at' => Carbon::now()]);
     }
 }
