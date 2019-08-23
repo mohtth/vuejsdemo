5e5cc6b220c6bf56b3251215d187018b745f919a
mohtth <mtoutouhi@gmail.com>
2019-08-23 18:02:01 +0200

    configuration passport

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-console.log('Salut Moh')
+fetch('/api/user', {
+    credentials: 'same-origin',
+    headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+    }
+})
